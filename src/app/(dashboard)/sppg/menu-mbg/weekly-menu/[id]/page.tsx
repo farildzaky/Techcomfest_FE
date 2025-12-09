49a0901478cf201eef72612a036c7a2de495ca33
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:34 +0700

    Add DetailMenuSppgPage component for displaying detailed menu in SPPG

---
+++ src/app/(dashboard)/sppg/menu-mbg/weekly-menu/[id]/page.tsx
@@ -0,0 +1,10 @@
+import DetailMenuSppg from "@/src/components/dashboard/sppg/menu-mbg/DetailMenu"
+
+const DetailMenuSppgPage = () => {
+    return(
+        <div>
+            <DetailMenuSppg/>
+        </div>
+    )
+}
+export default DetailMenuSppgPage
