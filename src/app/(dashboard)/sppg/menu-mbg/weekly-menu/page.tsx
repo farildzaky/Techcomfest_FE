a4681043671a04596f4feebd6c3483699d8f66ef
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:25 +0700

    Add WeeklyMenuPage component for displaying the weekly menu in SPPG

---
+++ src/app/(dashboard)/sppg/menu-mbg/weekly-menu/page.tsx
@@ -0,0 +1,11 @@
+import DaftarMenu from "@/src/components/dashboard/sppg/menu-mbg/DaftarMenu"
+
+const WeeklyMenuPage = () => {
+    return(
+        <div>
+            <DaftarMenu/>
+        </div>
+    )
+}
+
+export default WeeklyMenuPage
