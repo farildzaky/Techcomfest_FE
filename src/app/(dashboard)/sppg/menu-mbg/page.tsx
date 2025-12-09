6c185a772c4ac97f24e779816fe56fe77a5abb90
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:15 +0700

    Add MenuMbgPage component for SPPG menu management

---
+++ src/app/(dashboard)/sppg/menu-mbg/page.tsx
@@ -0,0 +1,10 @@
+import Option from "@/src/components/dashboard/sppg/menu-mbg/Option";
+
+const MenuMbgPage = () => {
+    return(
+        <div>
+            <Option/>
+        </div>
+    )
+}
+export default MenuMbgPage;
