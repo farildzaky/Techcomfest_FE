b890656310923d4074f024154c5467de16f17241
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:24:04 +0700

    Add ProfileInformasiSppgPage component for displaying institution information in SPPG

---
+++ src/app/(dashboard)/sppg/profile/informasi-instansi/page.tsx
@@ -0,0 +1,10 @@
+import InformasiInstansiPage from "../../../../../components/common/profile/sppg/informasi"
+
+const ProfileInformasiSppgPage = () => {
+    return (
+        <div >
+            <InformasiInstansiPage/>
+        </div>
+    )
+}
+export default ProfileInformasiSppgPage
