e0ce95f048e8f6493c9cec617b3fdf7963697b75
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:22 +0700

    Add DetailMenuSppgPage component for displaying detailed menu information

---
+++ src/app/(dashboard)/sekolah/menu-mbg/[id]/page.tsx
@@ -0,0 +1,10 @@
+import DetailMenu from "@/src/components/dashboard/sekolah/DetailMenu"
+
+const DetailMenuSppgPage = () => {
+    return(
+        <div>
+          <DetailMenu/>
+        </div>
+    )
+}
+export default DetailMenuSppgPage
