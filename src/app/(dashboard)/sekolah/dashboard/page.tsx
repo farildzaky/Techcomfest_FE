155ba5f562b64e4e352fce8381a48f5d6f2e0e34
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:12 +0700

    Add PelaporanPage component for school reporting dashboard

---
+++ src/app/(dashboard)/sekolah/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import MainDashboardSekolah from "@/src/components/dashboard/sekolah/Main";
+
+const PelaporanPage = () => {
+    return (
+        <div >
+            <MainDashboardSekolah />
+        </div>
+
+
+    );
+}
+export default PelaporanPage;
