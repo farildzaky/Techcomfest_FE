60e4693073911ae494b016ebcdec70af2c6c5baf
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:54 +0700

    Add RiwayatScanPage component for displaying scan history

---
+++ src/app/(dashboard)/sekolah/riwayat-scan/page.tsx
@@ -0,0 +1,10 @@
+import RiwayatScan from "@/src/components/dashboard/sekolah/scan-nutrisi/RiwayatScan";
+
+const RiwayatScanPage = () => {
+    return (
+        <div>
+            <RiwayatScan/>
+        </div>
+    );
+}
+export default RiwayatScanPage;
