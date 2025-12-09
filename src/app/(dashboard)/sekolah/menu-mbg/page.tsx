14bd729d333138772554ac64b310e5e17cf4559a
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:16 +0700

    Add MenuMbgPage component for school menu dashboard

---
+++ src/app/(dashboard)/sekolah/menu-mbg/page.tsx
@@ -0,0 +1,10 @@
+import MenuMbgSekolah from "@/src/components/dashboard/sekolah/MenuMbg";
+
+const MenuMbgPage = () => {
+    return (
+        <div>
+            <MenuMbgSekolah />
+        </div>
+    );
+}
+export default MenuMbgPage;
