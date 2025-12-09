30276445e12c5f9d4b66d7ce71a365644c722099
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:58 +0700

    Add ProfileSppgPage component for user profile page in SPPG

---
+++ src/app/(dashboard)/sppg/profile/data-laporan/page.tsx
@@ -0,0 +1,11 @@
+import SidebarProfileSppg from "@/src/components/common/profile/SidebarProfileSppg"
+
+const ProfileSppgPage = () => {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+export default ProfileSppgPage
