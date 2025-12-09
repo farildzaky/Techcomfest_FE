3617f425ceb52b3cd4df09273bb384b3ae3bac78
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:11 +0700

    Add DashboardSppgpage component for the SPPG dashboard layout

---
+++ src/app/(dashboard)/sppg/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import SidebarSppg from "@/src/components/common/sppg/Sidebar";
+import MainDashboardSppg from "@/src/components/dashboard/sppg/main";
+
+const DashboardSppgpage = () => {
+    return (
+        <div >
+
+                <MainDashboardSppg/>
+
+        </div>
+    );
+}
+export default DashboardSppgpage;
