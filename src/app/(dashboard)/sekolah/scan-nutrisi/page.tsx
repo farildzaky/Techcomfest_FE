4baea6861e2407d57b1f04ebb9e7d0aba2fca2f8
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:59 +0700

    Add ScanNutrisiPage component for displaying nutrition scan interface

---
+++ src/app/(dashboard)/sekolah/scan-nutrisi/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/src/components/common/Sidebar";
+import ScanNutrisiMain from "@/src/components/dashboard/sekolah/scan-nutrisi/main";
+
+const ScanNutrisiPage = () => {
+    return (
+        <div >
+            <ScanNutrisiMain/>
+        </div>
+        
+    );
+}
+export default ScanNutrisiPage;
