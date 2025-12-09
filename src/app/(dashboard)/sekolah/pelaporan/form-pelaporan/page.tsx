dbc92589cca6cbb94a2da0d69a79c39757c874a6
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:22:39 +0700

    Add PelaporanPage component for school reporting

---
+++ src/app/(dashboard)/sekolah/pelaporan/form-pelaporan/page.tsx
@@ -0,0 +1,11 @@
+import FormPelaporan from "@/src/components/dashboard/sekolah/pelaporan/Form";
+
+const PelaporanPage = () => {
+    return (
+        <div >
+            <FormPelaporan/>
+        </div>
+        
+    );
+}
+export default PelaporanPage;
