86b8bb7e7852613304aafd3b544a1fac8865f9d5
farildzaky <alfarildzaky@gmail.com>
2025-12-09 17:23:20 +0700

    Add IsiFormPage component for displaying the main menu in SPPG

---
+++ src/app/(dashboard)/sppg/menu-mbg/isi-menu/page.tsx
@@ -0,0 +1,10 @@
+import MainMenuMbg from "@/src/components/dashboard/sppg/menu-mbg/Main";
+
+const IsiFormPage = () => {
+    return(
+        <div>
+            <MainMenuMbg/>
+        </div>
+    )
+}
+export default IsiFormPage;
